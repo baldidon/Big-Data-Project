53674504c09615c24f8a8634fd81a9eeb39e1de0
CriBerna1703 <53260668+CriBerna1703@users.noreply.github.com>
2021-12-05 17:10:35 +0100

    Update UserTest.java

--- src/UserTest.java
+++ src/UserTest.java
@@ -73,6 +73,6 @@ public class UserTest {
 			}catch(IOException e){
 			}
 		}while(!input.equals("Q"));
-		System.out.println("Closing application. See you soon!!);
+		System.out.println("Closing application. See you soon!!");
 	}
 }
