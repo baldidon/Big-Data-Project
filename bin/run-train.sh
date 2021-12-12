b05df1351622b7ae4072bb0125ad53b4dcc0e4af
Andrea Baldinelli <48548880+baldidon@users.noreply.github.com>
2021-12-12 16:35:50 +0100

    Update run-train.sh

--- bin/run-train.sh
+++ bin/run-train.sh
@@ -1,27 +1,21 @@
 #!/bin/bash 
 
 
-rm -r /home/hadoopuser/esProf/output
 hdfs dfsadmin -safemode leave
 
-#hdfs dfs -rm -r dataset
+# delete all model 
 hdfs dfs -rm -r models
-#hdfs dfs -rm -r models/stopWordModel
-#hdfs dfs -rm -r models/countVectorizerModel
-#hdfs dfs -rm -r models/idfModel
-#hdfs dfs -rm -r models/NaiveBayesModel
+
+# delete output
 hdfs dfs -rm -r output
 
+# load dataset into hdfs
+hdfs dfs -put /home/hadoopuser/esProf/dataset dataset
 
-#hdfs dfs -put /home/hadoopuser/esProf/dataset dataset
-
+# create dir
 hdfs dfs -mkdir  models
-#hdfs dfs -mkdir  models/stopWordModel
-#hdfs dfs -mkdir  models/countVectorizerModel
-#hdfs dfs -mkdir  models/idfModel
-#hdfs dfs -mkdir  models/NaiveBayesModel
 
-
+# submit
 /usr/local/spark/bin/spark-submit --class bigdataman.baldiberna.naive_bayes_project.TrainModel --master yarn  --deploy-mode cluster \
  --num-executors 3 --driver-memory 2g  --executor-memory 3g  --executor-cores 2  --queue default \
 /home/hadoopuser/naive_bayes_project/naive_bayes_project.jar \
@@ -32,4 +26,8 @@ hdfs dfs -mkdir  models
 	/user/hadoopuser/models/NaiveBayesModel \
 	/user/hadoopuser/output
 
+
+# mkdir output
+
+# get accuracy model from hdfs
 hdfs dfs -get output /home/hadoopuser/output
